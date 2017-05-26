7bd58f850016fe85946fb162613c9915505baecd
JeffAbbin <abbin.1803@gmail.com>
2017-05-26 09:58:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,11 @@ public class main {
 	public static void main(String[] args) {
 		
 		 String nomeCliente;
+		 int poltrona;
+		 int fileira;
+		 int ContSessao = 0;
+		 int contMeia = 0;
+		 int contNormal = 0;
 		 Scanner nc = new Scanner(System.in);
 		 int escolhaFC = 0;
 		 System.out.print("Você é /n1 - Cliente /n2 - Funcionário?/n");
@@ -13,8 +18,8 @@ public class main {
 		 escolhaFC = fc.nextInt();
 		 Sessao s1 = new Sessao("Star Wars", 0);
 		 Sessao s2 = new Sessao("Guardiões da Galáxia", 1);
-		 Sessao s3 = new Sessao("Back to the Future", 3);
-		 Sessao s4 = new Sessao("Pulp Fiction", 4);
+		 Sessao s3 = new Sessao("Back to the Future", 2);
+		 Sessao s4 = new Sessao("Pulp Fiction", 3);
 		 
 		 do{
 			 switch(escolhaFC){
@@ -44,19 +49,48 @@ public class main {
 			 			 			switch(escolha3){
 			 			 				case 1:
 			 			 					escolha3 = 1;
-			 			 					System.out.print("14:00\n");		 			 					
+			 			 					System.out.print("14:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s1.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s1.lugares(fileira, poltrona);
+			 			 					
 			 			 					break;
 			 			 				case 2:
 			 			 					escolha3 = 2;
-			 			 					System.out.print("16:00\n");
+			 			 					System.out.print("16:00\n");System.out.print("escolha o lugar:\n");
+			 			 					s2.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s2.lugares(fileira, poltrona);
+			 			 					
 			 			 					break;
 			 			 				case 3:
 			 			 					escolha3 = 3;
-			 			 					System.out.print("16:00\n");			 			 					
+			 			 					System.out.print("16:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s3.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s3.lugares(fileira, poltrona);
 			 			 					break;
 			 			 				case 4:
 			 			 					escolha3 = 4;
 			 			 					System.out.print("20:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s4.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s4.lugares(fileira, poltrona);
 			 			 					break;
 			 			 				default:
 			 			 					System.out.print("Número Inválido\n");
@@ -77,18 +111,46 @@ public class main {
 			 			 				case 1:
 			 			 					escolha3 = 1;
 			 			 					System.out.print("14:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s1.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s1.lugares(fileira, poltrona);
 			 			 					break;
 			 			 				case 2:
 			 			 					escolha3 = 2;
 			 			 					System.out.print("16:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s2.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s2.lugares(fileira, poltrona);
 			 			 					break;
 			 			 				case 3:
 			 			 					escolha3 = 3;
 			 			 					System.out.print("16:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s3.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s3.lugares(fileira, poltrona);
 			 			 					break;
 			 			 				case 4:
 			 			 					escolha3 = 4;
 			 			 					System.out.print("20:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s4.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s4.lugares(fileira, poltrona);
 			 			 					break;
 			 			 				default:
 			 			 					System.out.print("Número Inválido\n");
@@ -109,18 +171,46 @@ public class main {
 			 			 				case 1:
 			 			 					escolha3 = 1;
 			 			 					System.out.print("14:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s1.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s1.lugares(fileira, poltrona);
 			 			 					break;
 			 			 				case 2:
 			 			 					escolha3 = 2;
 			 			 					System.out.print("16:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s2.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s2.lugares(fileira, poltrona);
 			 			 					break;
 			 			 				case 3:
 			 			 					escolha3 = 3;
 			 			 					System.out.print("16:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s3.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s3.lugares(fileira, poltrona);
 			 			 					break;
 			 			 				case 4:
 			 			 					escolha3 = 4;
 			 			 					System.out.print("20:00\n");
+			 			 					System.out.print("escolha o lugar:\n");
+			 			 					s4.mostraOcupado();
+			 			 					System.out.print("Digite fileira:");
+			 			 					fileira = e2.nextInt();
+			 			 					System.out.print("Digite poltrona:");
+			 			 					poltrona = e2.nextInt();
+			 			 					s4.lugares(fileira, poltrona);
 			 			 					break;
 			 			 				default:
 			 			 					System.out.print("Número Inválido\n");

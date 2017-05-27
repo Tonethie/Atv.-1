d38158bbc009ac56dc5c15c53cba344636cc82c2
JeffAbbin <abbin.1803@gmail.com>
2017-05-27 01:50:01 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,263 +1,220 @@
 package cinema;
 import java.util.Scanner;
 
-public class main {
+
+public class Main {
 
 	public static void main(String[] args) {
 		
-		 String nomeCliente;
-		 int poltrona;
-		 int fileira;
-		 int ContSessao = 0;
-		 int contMeia = 0;
-		 int contNormal = 0;
+		 String filme, nome;
+		 
+		 Cliente ls1[] = new Cliente[200];
+		 Cliente ls2[] = new Cliente[200];
+		 Cliente ls3[] = new Cliente[200];
+		 Cliente ls4[] = new Cliente[200];
+ 		 /* declarei novos clintes como sao 4 sessoes e cada
+ 		    sessao cabe 200 pessoa foi criado 4 vetores 
+ 		  */
+		 
+		 int S1contMeia = 0, S2contMeia = 0, S3contMeia = 0, S4contMeia = 0 ;
+		 int S1contNormal = 0, S2contNormal = 0, S3contNormal = 0, S4contNormal = 0;
+		 
+		 
 		 Scanner nc = new Scanner(System.in);
-		 int escolhaFC = 0;
-		 System.out.print("Você é /n1 - Cliente /n2 - Funcionário?/n");
-		 Scanner fc = new Scanner(System.in);
-		 escolhaFC = fc.nextInt();
+		 int escolha = 0, escolha1 = 0, x = 0,y = 0,z =0 ,w = 0, f, p, es;
+		 Scanner e = new Scanner(System.in);
 		 Sessao s1 = new Sessao("Star Wars", 0);
 		 Sessao s2 = new Sessao("Guardiões da Galáxia", 1);
 		 Sessao s3 = new Sessao("Back to the Future", 2);
 		 Sessao s4 = new Sessao("Pulp Fiction", 3);
-		 
+		 mostraMenu();
+		 escolha = e.nextInt();
 		 do{
-			 switch(escolhaFC){
-			 	case 1:
-			 		escolhaFC = 1; 
-			 		 System.out.print("--Cliente--/n");
-			 		 
-			 		 //pessoa p1 = new pessoa("Mariano", 1); //pessoa criada com tipo pagante Inteira
-			 		 //Sessao s1 = new Sessao("Star Wars", 0);
-			 		 
-			 		 int escolha2 = 0;
-			 		 int escolha3 = 0;
-			 		 System.out.print("Você é\n1 - Doador de sangue\n2 - Estudante\n3 - Normal\n");
-			 		 Scanner e2 = new Scanner(System.in);
-			 		 escolha2 = e2.nextInt();
-			 		 do{
-			 			 
-			 			 switch(escolha2){
-			 			 	case 1:
-			 			 		escolha2 = 1;
-			 			 		System.out.print("--Doador de sangue--\n");
-			 			 		System.out.print("Insira seu nome: ");
-			 			 		nomeCliente = nc.next();
-			 			 		pessoa p1 = new pessoa(nomeCliente, 2);
-			 			 		System.out.print("Escolha o horário da sua sessão:\n1 - 14:00\n2 - 16:00\n3 - 18:00\n4 - 20:00\n ");
-			 			 		do{
-			 			 			switch(escolha3){
-			 			 				case 1:
-			 			 					escolha3 = 1;
-			 			 					System.out.print("14:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s1.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s1.lugares(fileira, poltrona);
-			 			 					
-			 			 					break;
-			 			 				case 2:
-			 			 					escolha3 = 2;
-			 			 					System.out.print("16:00\n");System.out.print("escolha o lugar:\n");
-			 			 					s2.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s2.lugares(fileira, poltrona);
-			 			 					
-			 			 					break;
-			 			 				case 3:
-			 			 					escolha3 = 3;
-			 			 					System.out.print("16:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s3.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s3.lugares(fileira, poltrona);
-			 			 					break;
-			 			 				case 4:
-			 			 					escolha3 = 4;
-			 			 					System.out.print("20:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s4.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s4.lugares(fileira, poltrona);
-			 			 					break;
-			 			 				default:
-			 			 					System.out.print("Número Inválido\n");
-			 			 					break;
-			 			 			}
-			 			 		}while(escolha3 != 0);
-			 			 		break;
-			 			 		
-			 			 	case 2:
-			 			 		escolha2 = 2;
-			 			 		System.out.print("--Estudante--\n");
-			 			 		System.out.print("Insira seu nome: ");
-			 			 		nomeCliente = nc.next();
-			 			 		pessoa p2 = new pessoa(nomeCliente, 3);
-			 			 		System.out.print("Escolha o horário da sua sessão:\n1 - 14:00\n2 - 16:00\n3 - 18:00\n4 - 20:00\n ");
-			 			 		do{
-			 			 			switch(escolha3){
-			 			 				case 1:
-			 			 					escolha3 = 1;
-			 			 					System.out.print("14:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s1.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s1.lugares(fileira, poltrona);
-			 			 					break;
-			 			 				case 2:
-			 			 					escolha3 = 2;
-			 			 					System.out.print("16:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s2.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s2.lugares(fileira, poltrona);
-			 			 					break;
-			 			 				case 3:
-			 			 					escolha3 = 3;
-			 			 					System.out.print("16:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s3.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s3.lugares(fileira, poltrona);
-			 			 					break;
-			 			 				case 4:
-			 			 					escolha3 = 4;
-			 			 					System.out.print("20:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s4.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s4.lugares(fileira, poltrona);
-			 			 					break;
-			 			 				default:
-			 			 					System.out.print("Número Inválido\n");
-			 			 					break;
-			 			 			}
-			 			 		}while(escolha3 != 0);
-			 			 		break;
-			 			 		
-			 			 	case 3:
-			 			 		escolha2 = 3;
-			 			 		System.out.print("--Normal--\n");
-			 			 		System.out.print("Insira seu nome: ");
-			 			 		nomeCliente = nc.next();
-			 			 		pessoa p3 = new pessoa(nomeCliente, 1);
-			 			 		System.out.print("Escolha o horário da sua sessão:\n1 - 14:00\n2 - 16:00\n3 - 18:00\n4 - 20:00\n ");
-			 			 		do{
-			 			 			switch(escolha3){
-			 			 				case 1:
-			 			 					escolha3 = 1;
-			 			 					System.out.print("14:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s1.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s1.lugares(fileira, poltrona);
-			 			 					break;
-			 			 				case 2:
-			 			 					escolha3 = 2;
-			 			 					System.out.print("16:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s2.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s2.lugares(fileira, poltrona);
-			 			 					break;
-			 			 				case 3:
-			 			 					escolha3 = 3;
-			 			 					System.out.print("16:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s3.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s3.lugares(fileira, poltrona);
-			 			 					break;
-			 			 				case 4:
-			 			 					escolha3 = 4;
-			 			 					System.out.print("20:00\n");
-			 			 					System.out.print("escolha o lugar:\n");
-			 			 					s4.mostraOcupado();
-			 			 					System.out.print("Digite fileira:");
-			 			 					fileira = e2.nextInt();
-			 			 					System.out.print("Digite poltrona:");
-			 			 					poltrona = e2.nextInt();
-			 			 					s4.lugares(fileira, poltrona);
-			 			 					break;
-			 			 				default:
-			 			 					System.out.print("Número Inválido\n");
-			 			 					break;
-			 			 			}
-			 			 		}while(escolha3 != 0);
-			 			 		break;
-			 			 		
-			 			 	default:
-			 			 		System.out.print("--Número inválido--\n");
-			 			 		break;
-			 			 }
-			 			 
-			 		 }while(escolha2 != 0);
-			 		 break;
-			 		 
-			 	case 2:
-			 		escolhaFC = 2;
-			 		System.out.print("--Funcionário--");
-					System.out.println("Digite seu nome: \n");
-			 		nomeFunc = nf.next();
-			 		pessoa p4 = new pessoa("nomeFunc");
-			 		do{
-			 			System.out.println("Deseja:\n1 - Consultar lugar\n2 - Verificar lucro da sala\n3 - Alterar nome");
-			 			switch(escolha4){
-			 				case 1:
-			 					escolha4 = 1;
-			 					System.out.println("--Consultar Lugar--");
-			 					break;
-			 				case 2:
-			 					escolha4 = 2;
-			 					System.out.println("--Verificar Lucro da sala--");
-			 					break;
-			 				case 3:
-			 					escolha4 = 3;
-			 					break;
-			 				default:
-			 					System.out.println("Número Inválido");
-			 					System.out.println("--Alterar nome--");
-			 					break;
-			 			}
-			 		break;
-			 	default:
-			 		System.out.print("Número Inválido"); 
-			 		break;
+		 switch(escolha){
+		 
+		 case 1: // escolhe os filmes para as sessoes
+			 System.out.println("escolha filme para sessao das 14H");
+			  filme = nc.next();
+			  s1.setFilme(filme);
+			  System.out.println("escolha filme para sessao das 16H");
+			  filme = nc.next();
+			  s2.setFilme(filme);
+			  System.out.println("escolha filme para sessao das 18H");
+			  filme = nc.next();
+			  s3.setFilme(filme);
+			  System.out.println("escolha filme para sessao das 20H");
+			  filme = nc.next();
+			  s4.setFilme(filme);  
+		 break;
+		 
+		 case 2: /* FALTA CADASTRAR CLINTE COM NOME E SEU TIPO É SO COLOCAR O CONSTRUTOR DE CLIENTE
+		 ( na moral sinta-se a vontade pra excluir tudo isso pq nem EU to entendo oq ta acontecendo)*/
+			 
+			 escolhaSessao();
+			 escolha1 = nc.nextInt();
+			 switch(escolha1){
+			 case 1:
+				 
+				 s1.mostraOcupado();
+				 System.out.print("escolha fileira: ");
+				 f = nc.nextInt();
+				 System.out.print("\nescolha poltrona: ");
+				 p = nc.nextInt();
+				 s1.escolherLugar(ls1[x], f, p);
+				 x++;
+				 if(isinteira(ls1[x]) == true){
+					 S1contNormal++;
+				 }else{
+					 S1contMeia++;
+				 }
+			 break;
+			 case 2:
+				 s2.mostraOcupado();
+				 System.out.print("escolha fileira: ");
+				 f = nc.nextInt();
+				 System.out.print("\nescolha poltrona: ");
+				 p = nc.nextInt();
+				 s2.escolherLugar(ls2[y], f, p);
+				 y++;
+				 if(isinteira(ls2[y]) == true){
+					 S2contNormal++;
+				 }else{
+					 S2contMeia++;
+				 }
+				 
+				 break;
+			 case 3:
+				 s3.mostraOcupado();
+				 System.out.print("escolha fileira: ");
+				 f = nc.nextInt();
+				 System.out.print("\nescolha poltrona: ");
+				 p = nc.nextInt();
+				 s3.escolherLugar(ls3[z], f, p);
+				 z++;
+				 if(isinteira(ls3[z]) == true){
+					 S3contNormal++;
+				 }else{
+					 S3contMeia++;
+				 }
+				 break;
+			 case 4:
+				 s4.mostraOcupado();
+				 System.out.print("escolha fileira: ");
+				 f = nc.nextInt();
+				 System.out.print("\nescolha poltrona: ");
+				 p = nc.nextInt();
+				 s4.escolherLugar(ls4[w], f, p);
+				 w++;
+				 if(isinteira(ls4[w]) == true){
+					 S4contNormal++;
+				 }else{
+					 S4contMeia++;
+				 }
+				 break;
 			 }
-		 }while(escolhaFC != 0);
+			 break;
+			 
+			 
+		 case 3: /* checa as pessoas sentadas em cada sessao pela 
+		 poltrona e fileiras, as sessos sao escolhida  pelo switch e AH BLA BLA BLA BLA BLA EU ODEIO DOCUMENTAR CODIGO AHHHHHH, Giovanna
+		 se endendeu essa parte!! */
+			 
+			 escolhaSessao();
+			 escolha = nc.nextInt();
+			 switch(escolha){
+			 case 1:
+				 System.out.print("escolha fileira: ");
+				 f = nc.nextInt();
+				 System.out.print("\nescolha poltrona: ");
+				 p = nc.nextInt();
+				 s1.checkAssento(f, p);
+				 break;
+			 case 2:
+				 System.out.print("escolha fileira: ");
+				 f = nc.nextInt();
+				 System.out.print("\nescolha poltrona: ");
+				 p = nc.nextInt();
+				 s2.checkAssento(f, p);
+				 break;
+			 case 3:
+				 System.out.print("escolha fileira: ");
+				 f = nc.nextInt();
+				 System.out.print("\nescolha poltrona: ");
+				 p = nc.nextInt();
+				 s3.checkAssento(f, p);
+				 break;
+			 case 4:
+				 System.out.print("escolha fileira: ");
+				 f = nc.nextInt();
+				 System.out.print("\nescolha poltrona: ");
+				 p = nc.nextInt();
+				 s4.checkAssento(f, p);
+				 break;
+			 }
+			 break;
+		 case 4: // aki é pra mostrar os resultados financeiro de cada sessao, por isso esses cases ae
+			 escolhaSessao();
+			 escolha = nc.nextInt();
+			 switch(escolha){
+			 case 1:
+				 
+				 break;
+			 case 2:
+				 
+				 break;
+			 case 3:
+				 
+				 break;
+			 case 4:
+				 
+				 break;
+			 
+			 }
+			 break;
+		 case 5: // resultados financeiros de todas as sessoes, é so somar tudo
+			 break;
+		 default:
+				System.out.print("Número Inválido\n");
+				break;
+		  }
+		 
+		 }while(escolha < 1 && escolha > 5);
+	
+	
 	}
-
+	
+	
+	public static void mostraMenu(){ // é so uma função pra menu
+		System.out.print("-------------------------------------\n");
+		System.out.println("1- Cadastrar nome do filme para cada sessao ");
+		System.out.println("2- compre seu ingresso ");
+		System.out.println("3- Ocupaçao de cada sessao e consuta de cada poltrona");
+		System.out.println("4- mostrar lucro da bileteria de determina sessao ");
+		System.out.println("5- mostrar lucro da bileteria geral ");
+		System.out.print("--------------------------------------\n ");
+	}
+	
+	public static void escolhaSessao(){ // é so uma função pra menu
+		System.out.print("-------------------------------------\n");
+		System.out.println("1- sessao das 14h ");
+		System.out.println("2- sessao das 16h ");
+		System.out.println("3- sessao das 18h");
+		System.out.println("4- sessao das 20h ");
+		System.out.print("--------------------------------------\n ");
+	}
+	
+	public static boolean isinteira(Cliente c1){ /* quando invocada consome a stamina do seu oponente... malz nao é sykrim aki nao
+		seriao agora, quando invocada pega um cliente e verifica se seu ingreso é meia ou inteira( se for inteira retorna TRUE se nao FLASE */ 
+		if(c1.getTipo() == "inteira"){           
+		return true;
+		}else{
+			return false;
+		}
+	}
+	
+	
+		
+		
+	
+	
 }
